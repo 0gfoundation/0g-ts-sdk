78a4ef07c1c4d590cc011200e2da1c72b1589f4e
Peter Zhang <peter@0g.ai>
2025-12-10 19:17:34 +0800

    removed compiled files

--- src.ts/file/ZgFile.ts
+++ src.ts/file/ZgFile.ts
@@ -46,7 +46,7 @@ export class ZgFile extends AbstractFile {
         start: number,
         end: number
     ): Promise<{ bytesRead: number; buffer: Uint8Array }> {
-        if (start < 0 || start >= this.size()) {
+        if (start < 0 || start >= this.size() || start >= end) {
             throw new Error('invalid start offset')
         }
         if (end > this.size()) {
